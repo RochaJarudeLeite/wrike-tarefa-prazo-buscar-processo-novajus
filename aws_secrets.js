6b996b3ae28be406b5ffce989da8d91f662570fe
Luiz Rocha <luiz.rocha@rjladv.com>
2022-08-13 18:34:52 -0500

    new try 6

--- aws_secrets.js
+++ aws_secrets.js
@@ -6,10 +6,8 @@ const secretsClient = new SecretsManagerClient({region: REGION})
 
 const client = new SecretsManagerClient({
     region: REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
 async function getSecret(params) {

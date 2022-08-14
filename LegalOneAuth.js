11071f1ed8cdada6bbba41d65600eaf071578f83
Luiz Rocha <luiz.rocha@rjladv.com>
2022-08-13 19:25:41 -0500

    new try 9

--- LegalOneAuth.js
+++ LegalOneAuth.js
@@ -12,11 +12,11 @@ let tokenInfoFile = 'LegalOneTokenInfo.json'
 let secret = await getSecret(params);
 let legalOneKey = secret.THOMSON_REUTERS_TOKEN;
 
-// check if ./tmp/LegalOneTokenInfo.json exists and if not download it
+// check if .tmp/LegalOneTokenInfo.json exists and if not download it
 let tokenInfo = {"ExpirationDate": ""};
 try {
-    console.log("Reading Token info from tmp/LegalOneTokenInfo.json")
-    tokenInfo = JSON.parse(fs.readFileSync('tmp/' + tokenInfoFile))
+    console.log("Reading Token info from .tmp/LegalOneTokenInfo.json")
+    tokenInfo = JSON.parse(fs.readFileSync('.tmp/' + tokenInfoFile))
     console.log("Token info: " + JSON.stringify(tokenInfo))
 } catch (err) {
     console.log("Couldn't read. Downloading file on S3")
@@ -53,8 +53,8 @@ async function getToken(forced = false) {
         let dt = new Date(parseInt(body.issued_at))
         dt.setSeconds(dt.getSeconds() + parseInt(body.expires_in))
         tokenInfo.ExpirationDate = dt
-        fs.writeFileSync('tmp/LegalOneTokenInfo.json', JSON.stringify(tokenInfo))
-        await S3.uploadFileS3('tmp/LegalOneTokenInfo.json');
+        fs.writeFileSync('.tmp' + tokenInfoFile, JSON.stringify(tokenInfo))
+        await S3.uploadFileS3('.tmp' + tokenInfoFile);
         legalOneKey = body.access_token;
         await setSecret(body.access_token);
         console.log("Token updated.");
